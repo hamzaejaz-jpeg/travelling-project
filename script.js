e7fdb294050d3644c3221f8ccae15b670982f51c
hamzaejaz-jpeg <ehamza0018@gmail.com>
2025-08-16 15:56:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault(); // prevent page reload
+  alert("Thank you for your message! We'll get back to you soon.");
+});
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
